c2c66b8b724f8b9b3105699531928af8b64df897
Abdul Al kadri <187457@stud.hvl.no>
2024-09-03 14:24:53 +0200

    Update OppgaveV2Lab2.java

--- programmering/jplab2/DAT100Lab2/ValgfrieOppgaverLab2/OppgaveV2Lab2.java
+++ programmering/jplab2/DAT100Lab2/ValgfrieOppgaverLab2/OppgaveV2Lab2.java
@@ -3,14 +3,57 @@ package ValgfrieOppgaverLab2;
 import easygraphics.EasyGraphics;
 
 public class OppgaveV2Lab2 extends EasyGraphics {
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void run() {
+        int windowWidth = 600;
+        int windowHeight = 400;
+        makeWindow("Kjønnsfordeling i Skoleklasser", windowWidth, windowHeight);
 
-        setColor(0, 0, 0);
-        fillRectangle(50, 50, 300, 60);
+        // Klasse A
+        int percentA = 28; // Prosentandel av gutter
+        int greenWidthA = (windowWidth * percentA) / 100;
+        int redWidthA = windowWidth - greenWidthA;
+
+        setColor(0, 255, 0); // Grønn farge for gutter
+        fillRectangle(50, 50, greenWidthA, 100); // Tegn grønt rektangel
+
+        setColor(255, 0, 0); // Rød farge for jenter
+        fillRectangle(50 + greenWidthA, 50, redWidthA, 100); // Tegn rødt rektangel
+
+        setColor(0, 0, 0); // Sort farge for ramme
+        drawRectangle(50, 50, windowWidth, 100); // Tegn ramme rundt
+
+        // Klasse B
+        int percentB = 38; // Prosentandel av gutter
+        int greenWidthB = (windowWidth * percentB) / 100;
+        int redWidthB = windowWidth - greenWidthB;
+
+        setColor(0, 255, 0); // Grønn farge for gutter
+        fillRectangle(50, 180, greenWidthB, 100); // Tegn grønt rektangel
+
+        setColor(255, 0, 0); // Rød farge for jenter
+        fillRectangle(50 + greenWidthB, 180, redWidthB, 100); // Tegn rødt rektangel
+
+        setColor(0, 0, 0); // Sort farge for ramme
+        drawRectangle(50, 180, windowWidth, 100); // Tegn ramme rundt
+
+        // Klasse C
+        int percentC = 46; // Prosentandel av gutter
+        int greenWidthC = (windowWidth * percentC) / 100;
+        int redWidthC = windowWidth - greenWidthC;
+
+        setColor(0, 255, 0); // Grønn farge for gutter
+        fillRectangle(50, 310, greenWidthC, 100); // Tegn grønt rektangel
+
+        setColor(255, 0, 0); // Rød farge for jenter
+        fillRectangle(50 + greenWidthC, 310, redWidthC, 100); // Tegn rødt rektangel
+
+        setColor(0, 0, 0); // Sort farge for ramme
+        drawRectangle(50, 310, windowWidth, 100); // Tegn ramme rundt
     }
 }
